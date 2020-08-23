6f8e2b4c99d83a92e40f9d151904f587fa725ab4
Jonathan <yoniyaniv@gmail.com>
2020-08-23 11:53:02 -0400

    .

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const host = (process.env && process.env.HOST) || 'localhost';
 
 const app = express()
 
-app.use(express.static(path.join(__dirname, 'frontend')));
+app.use(express.static(path.join(__dirname, 'frontend/public')));
 
 app.use('/api', require('./routes/api'))
 
